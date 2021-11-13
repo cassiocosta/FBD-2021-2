fe565ab898f438d802786ae7c7029eef74f2b73e
Cassio Costa <cassio.hc@gmail.com>
2021-11-12 21:44:39 -0300

    solução exercícios

--- Aula19-Joins.sql
+++ Aula19-Joins.sql
@@ -120,22 +120,80 @@ order by
 limit 
 
 
--- 1 - todas todos os pacientes do médico RAfael hoje
+-- 1 - todos os pacientes que o médico RAfael tem para atender hoje
+select  p.nome as pacientes
+from    pacientes p
+        inner join consultas c
+          on p.id = c.paciente_id
+        inner join medicos m
+          on m.id = c.medico_id
+where   data_consulta = '2021-11-12' and m.nome like "Rafael%"
+
+and m.id = 1225
 
 -- 2 - todos os pacientes que temos para atender hoje
 
+select  p.*
+from     pacientes p
+            inner join consultas c
+                on p.id = c.paciente_id
+where   data_consulta = current_date;
+
 -- 3 - valor total das consultas do mês de junho 2021
+--- funções de agregação: sum, count, max, min, avg... 
+select  SUM(valor)  as total_consultas
+from    consultas c
+where   c.data between '2021-06-01' and '2021-06-30'
+
 
 -- 4- todos os pacientes da Médica Júlia e de qual cidade eles pertencem 
--- que tem consulta no mês de dezembro de 2021
+-- que tem consulta no mês de dezembro de 2021 (codigo da júlia é 6565)
+-- versao 1
+select  p.*
+from pacientes p
+        inner join consultas c 
+            on c.paciente_id = p.id
+where c.medico_id = 6565 and c.data between '2021-12-01' and '2021-12-31'
 
-
+-- versão
+select  p.*
+from pacientes p
+        inner join consultas c 
+            on c.paciente_id = p.id
+        inner join medicos m
+            on m.id = c.medico_id
+where m.nome like 'Julia%' and c.data between '2021-12-01' and '2021-12-31'
 
 -- 5 - Todos os médicos que já atenderam algum paciente da cidade de Torres.
+
+
+select medicos.nome as nome_medico, pacientes.nome as nome_paciente, cidades.nome as cidade
+from consultas
+	join medicos
+    	on medicos.id = consultas.medico_id
+    join pacientes
+    	on pacientes.id = consultas.paciente_id
+    join cidades
+    	on cidades.id=pacientes.cidade_id
+where pacientes.cidade_id = 3;
+
+SELECT medicos.nome 
+FROM consultas 
+	INNER JOIN medicos ON consultas.medico_id = medicos.id
+    INNER JOIN pacientes on consultas.paciente_id = pacientes.id
+	inner JOIN cidades on pacientes.cidade_id = cidades.id
+WHERE cidades.nome = 'Torres'
 
 -- 6 Médico: Atendente, pf. ve qual foi a última vez que eu atendi a Rafaela e 
 -- de qual cidade ela é?
 
-
+-- o código da rafa é 3456
+select  paciente_id, data, medico_id, cid.nome as nome_da_cidade
+from    consultas c
+            inner join pacientes p
+                on p.id = c.paciente_id
+            inner join  cidades cid
+                on cid.id = p.cidade_id
+where   id = 3456
 
 
